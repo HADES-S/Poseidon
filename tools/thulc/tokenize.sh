2cdf5163fb381476cdec0a72af6368836f44b253
Seu_SYH <Seu_SYH>
2018-02-01 21:42:53 +0800

    完成目录切换
    Signed-off-by: Seu_SYH <Seu_SYH>

--- tools/thulc/tokenize.sh
+++ tools/thulc/tokenize.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -ueo pipefail
 
+cd ~/syh/test/Poseidon/tools/thulc
+
 for AMR in "${TRAIN_FILE}" "${DEV_FILE}" "${TEST_FILE}"
 do
     java -jar ${thulc_JAR} -t2s -seg_only -deli " " $*> "${AMR}.snt.token"
